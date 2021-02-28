1c73579c20046d3144e89cf26e87efb692e360a2
shahrukhx01 <sk28671@gmail.com>
2021-02-28 14:59:28 +0100

    add front-end scraper

---
+++ frontend_scraper/scraper.js
@@ -0,0 +1,11 @@
+// simulates click to populate all text
+document.getElementsByClassName('show-more-less-html__button show-more-less-html__button--more')[0].click()
+
+function download(text, name, type) {
+
+  var a = document.getElementsByTagName('a')[0]; // get proxy link element
+  var file = new Blob([text], {type: type}); // create file blob with content
+  a.href = URL.createObjectURL(file); // add object URL via proxy link
+  a.download = name; // add download attribute to set file name
+  a.click() // simulate click
+}
