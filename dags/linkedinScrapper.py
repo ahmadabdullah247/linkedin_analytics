79c7b29f0f7b8e298eca3213de6d326183f7c733
ahmadabdullah247 <ahmadabdullah247@live.com>
2021-03-03 06:29:08 +0100

    Scraping workflow

--- dags/linkedinScrapper.py
+++ dags/linkedinScrapper.py
@@ -1,11 +1,16 @@
 from airflow import DAG
 from airflow.utils.dates import days_ago
-from airflow.operators.bash_operator import BashOperator
-from airflow.operators.email_operator import EmailOperator
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.subdag_operator import SubDagOperator
 
 import requests
 from bs4 import BeautifulSoup
+
+# db setup 
+# https://airflow.apache.org/docs/apache-airflow/stable/howto/set-up-database.html
+# for authentication
+# https://stackoverflow.com/questions/52056809/how-to-activate-authentication-in-apache-airflow/52057433
 
 # Set default args
 default_args = {
@@ -28,6 +33,39 @@ dag = DAG(
   description= 'Getting Job Description from Linkedin',
   default_args= default_args,
   schedule_interval= schedule_interval)
+
+
+def subdag(parent_dag_name, child_dag_name, args):
+    """
+    Generate a DAG to be used as a subdag.
+
+    :param str parent_dag_name: Id of the parent DAG
+    :param str child_dag_name: Id of the child DAG
+    :param dict args: Default arguments to provide to the subdag
+    :return: DAG to use as a subdag
+    :rtype: airflow.models.DAG
+    """
+    dag_subdag = DAG(
+        dag_id=f'{parent_dag_name}.{child_dag_name}',
+        default_args=default_args,
+        start_date=days_ago(2),
+        schedule_interval="@daily",
+    )
+
+    with dag_subdag:
+        task_SE_job_ids = PythonOperator(
+            task_id='Software_Engineer_Job_Ids', 
+            python_callable=get_job_ids, 
+            provide_context=True,
+            dag=dag_subdag)
+
+        task_DE_job_ids = PythonOperator(
+            task_id='Data_Engineer_Job_Ids', 
+            python_callable=get_job_ids, 
+            provide_context=True,
+            dag=dag_subdag)
+
+    return dag_subdag
 
 
 def get_job_ids(**context):
@@ -64,27 +102,38 @@ def get_job_description(**context):
 
     return job_descriptions
 
+def consolidating_job_ids(**context):
+    # get job ids form all sub-tasks
+    job_ids = context['ti'].xcom_pull(key='jobIds')
+    
+    # save only unique from them
+    # Take out id's that are already in database 
+
 
 with dag:
+    start = DummyOperator(task_id="start")
+
     # Task 1: scraping job ids from linkedin
-    task_get_job_ids = PythonOperator(
-        task_id='Get_Job_Ids', 
-        python_callable=get_job_ids, 
+    task_get_job_ids = SubDagOperator(
+        task_id='Get_Job_Ids',
+        subdag=subdag('Linkedin_Scrapper', 'Get_Job_Ids', default_args),
         provide_context=True,
         dag=dag)
 
-    # Task 2: scraping job description for job ids
+    # Task 2: Only keeping unique ids
+    task_consolidating_job_ids= PythonOperator(
+        task_id='Consolidating_Job_Ids', 
+        python_callable=consolidating_job_ids, 
+        provide_context=True,
+        dag=dag)
+
+    # Task 3: scraping job description for job ids
     task_get_job_description = PythonOperator(
         task_id='Get_Job_Description', 
         python_callable=get_job_ids, 
         provide_context=True,
         dag=dag)
 
+    end = DummyOperator(task_id="end")
 
-    # Echo task finish
-    task_finish = BashOperator(
-        task_id = 'finish_task', 
-        bash_command = 'echo finish', 
-        dag = dag)
-
-    task_get_job_ids >> task_get_job_description >> task_finish
+    start >> task_get_job_ids >> task_consolidating_job_ids >> task_get_job_description >> end
